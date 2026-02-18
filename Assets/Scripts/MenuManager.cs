f34d02a2c8317e57e1f340cbf4a67f0de8f7e1e7
Soham <sohamkagrawal@gmail.com>
2026-02-17 20:42:37 -0500

    Adding a main menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("CollectTheWood");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("LavaLevel");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
