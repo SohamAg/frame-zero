fd653650fe118c1193fd9af36883914754732caa
kb8767 <krishnahbhanderi@gmail.com>
2026-04-25 01:25:19 -0400

    defense

--- Assets/BossLevel/Scripts/PlayerHealth.cs
+++ Assets/BossLevel/Scripts/PlayerHealth.cs
@@ -1,33 +1,40 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
 
-    public Slider healthBar;
+    public int potionHeal = 20;
+    private bool potionUsed = false;
+
+    private PlayerDefense defense;
 
     void Start()
     {
         currentHealth = maxHealth;
+        defense = GetComponent<PlayerDefense>();
+    }
 
-        if (healthBar != null)
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            healthBar.maxValue = maxHealth;
-            healthBar.value = currentHealth;
+            UsePotion();
         }
     }
 
     public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
-
-        if (healthBar != null)
+        // Block damage if defending
+        if (defense != null && defense.isDefending)
         {
-            healthBar.value = currentHealth;
+            Debug.Log("Blocked damage!");
+            return;
         }
+
+        currentHealth -= damage;
+        Debug.Log("Health: " + currentHealth);
 
         if (currentHealth <= 0)
         {
@@ -35,10 +42,24 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
+    void UsePotion()
+    {
+        if (potionUsed)
+        {
+            Debug.Log("Potion already used");
+            return;
+        }
+
+        currentHealth += potionHeal;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        potionUsed = true;
+
+        Debug.Log("Potion used! Health: " + currentHealth);
+    }
+
     void Die()
     {
-        Debug.Log("Player Dead");
-
-        // add animation for death?
+        Debug.Log("Player died");
     }
 }
