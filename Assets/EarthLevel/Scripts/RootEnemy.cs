0ca0b74384223f0f88a209c556d5675e00b3fe25
nk445 <143555609+nk445@users.noreply.github.com>
2026-04-26 01:18:41 -0400

    Implement enemy ai states, basing speed on distance from player

--- Assets/EarthLevel/Scripts/RootEnemy.cs
+++ Assets/EarthLevel/Scripts/RootEnemy.cs
@@ -5,19 +5,84 @@ public class RootEnemy : MonoBehaviour
     public Transform player;
     public float speed;
 
+    // distance threshold for ai states
+    private float slowThreshold = 20f;
+    private float fastThreshold = 10f;
+
+    // speeds for states
+    private float fastSpeed = 13f;
+    private float slowSpeed = 9f;
+    private float patrolSpeed = 4f;
+    public enum AIState
+    {
+        Patrol,
+        ChaseSlow,
+        ChaseFast
+    };
+    public AIState aiState;
+
     private void Start()
     {
-        speed = 10f;
+        aiState = AIState.Patrol;
+        speed = 2f;
     }
 
     void Update()
     {
         if (player == null) return;
 
-        // 1. Look at player
+        // Look at player
         transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
 
-        // 2. Move towards player
+        // calculate distance from player to determine state
+        float distance_from_player = Vector3.Distance(transform.position, player.position);
+
+        switch (aiState)
+        {
+            
+            case AIState.Patrol:
+                speed = patrolSpeed;
+                if (distance_from_player < fastThreshold)
+                {
+                    aiState = AIState.ChaseFast;
+                }
+                else if (distance_from_player < slowThreshold)
+                {
+                    aiState = AIState.ChaseSlow;
+                }
+                break;
+
+            case AIState.ChaseSlow:
+                speed = slowSpeed;
+                if (distance_from_player < fastThreshold)
+                {
+                    aiState = AIState.ChaseFast;
+                }
+                if (distance_from_player > slowThreshold)
+                {
+                    aiState = AIState.Patrol;
+                }
+                break;
+
+            case AIState.ChaseFast:
+                speed = fastSpeed;
+                if (distance_from_player > slowThreshold)
+                {
+                    aiState = AIState.Patrol;
+                }
+                else if (distance_from_player > fastThreshold)
+                {
+                    aiState = AIState.ChaseSlow;
+                }
+                break;
+            default:
+                speed = 2f;
+                aiState = AIState.Patrol;
+                break;
+        }
+        
+
+        // Move towards player
         transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
 }
